710eea5bff4e27215d33367b86cac452a47e58a4
nemca01 <154055713+nemca01@users.noreply.github.com>
2024-11-24 17:10:13 +0100

    Update projekat.js

--- projekat.js
+++ projekat.js
@@ -33,3 +33,9 @@ document.getElementById("prikazi-proizvode").addEventListener("mouseover", funct
 function kupovina(){
     alert('Uspešno ste ubacili u korpu ovu igricu!');
 }
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B13KQL7RTB"></script>
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B13KQL7RTB');
